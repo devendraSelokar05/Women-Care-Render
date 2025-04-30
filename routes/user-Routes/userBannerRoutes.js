b50659080926465363fc7de467c93ba47389bd26
Devendra Selokar <selokardevendra05@gmail.com>
2025-04-30 10:10:19 +0530

    Delete routes directory

--- routes/user-Routes/userBannerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getSingleBanner } = require("../../controllers/UserControllers/userBannerController");
-
-const router = express.Router();
-
-//✅ User Banner Routes
-router.get("/getBanner", getSingleBanner);
-
-module.exports = router;
