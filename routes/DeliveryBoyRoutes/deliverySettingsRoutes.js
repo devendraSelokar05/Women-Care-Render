5c8af5249cd2b604bc1470236d592b33975238af
Devendra Selokar <selokardevendra05@gmail.com>
2025-05-01 14:22:46 +0530

    Delete routes directory

--- routes/DeliveryBoyRoutes/deliverySettingsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getDeliveryBoyTermsandCondition } = require("../../controllers/deliveryBoy-Controllers/delieveryBoysettingsController");
-
-const router = express.Router();
-
-
-router.get("/getSettings", getDeliveryBoyTermsandCondition);
-
-module.exports = router;
