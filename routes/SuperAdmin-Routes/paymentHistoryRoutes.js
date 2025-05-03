602e6589a26f5a44e52413c3daf684b978a3b142
Devendra Selokar <selokardevendra05@gmail.com>
2025-05-03 10:53:48 +0530

    Delete routes directory

--- routes/SuperAdmin-Routes/paymentHistoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getPaymentHistory, viewPaymentByDeliveryBoy, getPaymentStatus } = require('../../controllers/SuperAdmin-Controllers/paymentHistoryController');
-
-const router = express.Router();
-
-router.get('/payment-history', getPaymentHistory);
-router.get('/payment-history/:deliveryBoyId', viewPaymentByDeliveryBoy);
-router.get('/payment-status', getPaymentStatus);
-module.exports = router;
