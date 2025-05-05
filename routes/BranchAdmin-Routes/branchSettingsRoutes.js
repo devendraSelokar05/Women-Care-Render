8c7337e12b47389d4467faab0ea1b0312a993fb5
Devendra Selokar <selokardevendra05@gmail.com>
2025-05-05 16:46:42 +0530

    Delete routes directory

--- routes/BranchAdmin-Routes/branchSettingsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getSettings } = require("../../controllers/branchAdmin-Controllers/branchSettingsController");
-const router = express.Router();
-
-
-//✅ Branch Settings Routes
-router.get("/getSettings", getSettings);
-
-module.exports = router;
