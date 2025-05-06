e7e28024a198a0ded6a09d73a7488274292c3c7f
Devendra Selokar <selokardevendra05@gmail.com>
2025-05-06 10:35:24 +0530

    Delete routes directory

--- routes/BranchAdmin-Routes/branchAdminDetailsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getBranchDetails } = require("../../controllers/branchAdmin-Controllers/branchDetailsController");
-
-//✅ Branch Details Routes
-router.get("/branch/:branchId", getBranchDetails);
-
-module.exports = router;
--- routes/user-Routes/userBannerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getAllBanners } = require("../../controllers/UserControllers/userBannerController");
-
-const router = express.Router();
-
-//✅ User Banner Routes
-router.get("/getAllBanners", getAllBanners);
-
-module.exports = router;
